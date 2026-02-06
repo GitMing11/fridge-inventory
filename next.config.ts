9feef646eeb489dd2730ec62fc4a71c5290916c2
Ming <132457273+GitMing11@users.noreply.github.com>
2026-02-07 03:27:27 +0900

    refactor: 홈 페이지 UI 개선 및 스타일 수정

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,19 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: 'lh3.googleusercontent.com', // 구글 프로필 이미지
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https', // 카카오 이미지
+        hostname: 'k.kakaocdn.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http', // 카카오 이미지
+        hostname: 'k.kakaocdn.net',
         port: '',
         pathname: '/**',
       },
